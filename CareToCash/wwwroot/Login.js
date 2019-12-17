d9b1c914b46654a9a41178c2b18522ec70ef5a5e
Kam Parsen <kamparsen@yahoo.com>
2019-12-17 15:25:26 -0500

    added login

--- CareToCash/wwwroot/Login.js
+++ CareToCash/wwwroot/Login.js
@@ -7,12 +7,21 @@
 
 function RegisterUser(email, password) {
     //alert("email:" + email + " " + "password:" + password);
-    firebase.auth()..createUserWithEmailAndPassword(email, password).catch (function(error) {
-        // Handle Errors here.
-       // var errorCode = error.code;
-      //  var errorMessage = error.message;
-        alert(error.message)
-    });
+    //firebase.auth().createUserWithEmailAndPassword(email, password).catch (function(error) {
+    //    // Handle Errors here.
+    //   // var errorCode = error.code;
+    //  //  var errorMessage = error.message;
+    //    alert(error.message)
+    //});
+
+
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(function (result) {
+            alert(result);
+        }).catch(function (error) {
+            alert(error);
+        });
 
 }
 
