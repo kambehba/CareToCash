5ffc5d3cb5d66d67d821099218321cf52db91687
Kam Parsen <kamparsen@yahoo.com>
2019-12-18 10:12:08 -0500

    added and completed Login

--- CareToCash/wwwroot/index.js
+++ CareToCash/wwwroot/index.js
@@ -6,19 +6,10 @@
 
 
 function RegisterUser(email, password) {
-    //alert("email:" + email + " " + "password:" + password);
-    //firebase.auth().createUserWithEmailAndPassword(email, password).catch (function(error) {
-    //    // Handle Errors here.
-    //   // var errorCode = error.code;
-    //  //  var errorMessage = error.message;
-    //    alert(error.message)
-    //});
-
-
-
+    
     firebase.auth().createUserWithEmailAndPassword(email, password)
         .then(function (result) {
-            alert(result);
+            alert("User Registered Successfully");
         }).catch(function (error) {
             alert(error);
         });
---
+++ CareToCash/wwwroot/init-firebase.js
@@ -0,0 +1,19 @@
+﻿
+//var firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+//require("firebase/auth");
+//require("firebase/firestore");
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDYiYRMlAZpEXya9aVUhv9cpJwJL4Oz7gM",
+    authDomain: "dazzling-torch-8270.firebaseapp.com",
+    databaseURL: "https://dazzling-torch-8270.firebaseio.com",
+    projectId: "dazzling-torch-8270",
+    storageBucket: "dazzling-torch-8270.appspot.com",
+    messagingSenderId: "935228019520",
+    appId: "1:935228019520:web:0d827d87281eb216e920a5"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
